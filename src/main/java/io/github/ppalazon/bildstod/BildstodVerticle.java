2684077b260bca4d65360ca1e21b2e1fbdcc3588
Pablo Palazón <ppalazon@antara.ws>
2016-10-21 15:46:07 +0200

    First draft of bildstod project

---
+++ src/main/java/io/github/ppalazon/bildstod/BildstodVerticle.java
@@ -0,0 +1,17 @@
+package io.github.ppalazon.bildstod;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * User: ppalazon
+ * Date: 10/21/16
+ * Time: 2:01 PM
+ */
+public class BildstodVerticle extends AbstractVerticle
+{
+    @Override
+    public void start() throws Exception
+    {
+
+    }
+}
